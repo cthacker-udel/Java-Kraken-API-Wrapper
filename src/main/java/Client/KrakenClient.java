5f86ab78b4f59286d74a9d87fb2a4f5738f48d7c
cthacker-udel <cthacker@udel.edu>
2021-05-31 03:26:52 -0400

    Added : Method implementation for encoding and decoding values in Base64 and UTF-8

--- src/main/java/Client/KrakenClient.java
+++ src/main/java/Client/KrakenClient.java
@@ -1,5 +1,12 @@
 package Client;
 
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class KrakenClient {
 
     KrakenKeys apiKeys;
@@ -10,6 +17,29 @@ public class KrakenClient {
 
     public KrakenClient(String apiKey, String secretKey){
         this.apiKeys = new KrakenKeys(apiKey,secretKey);
+    }
+
+    public KrakenKeys getApiKeys() {
+        return apiKeys;
+    }
+
+    public void setApiKeys(KrakenKeys apiKeys) {
+        this.apiKeys = apiKeys;
+    }
+
+    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, MalformedURLException {
+
+        KrakenClient client = new KrakenClient("","kQH5HW/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg==");
+        Long nonce = 1616492376594L;
+        Map<String,Object> payload = new LinkedHashMap<>();
+        payload.put("nonce",nonce);
+        payload.put("ordertype","limit");
+        payload.put("pair","XBTUSD");
+        payload.put("price",37500);
+        payload.put("type","buy");
+        payload.put("volume",1.25);
+        System.out.println(client.getApiKeys().generateSignature(payload));
+
     }
 
 }
--- src/main/java/Client/KrakenKeys.java
+++ src/main/java/Client/KrakenKeys.java
@@ -1,20 +1,41 @@
 package Client;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 
-public class KrakenKeys extends KrakenClient{
+public class KrakenKeys extends KrakenClient {
 
+    private static final String HMAC_SHA512 = "HmacSHA512";
+    private static final String SHA256 = "SHA-256";
+    private static final String UTF8 = "UTF-8";
+    private static final String AMPERSAND = "&";
+    private static final String EQUAL_SIGN = "=";
+    private static final String ERROR_NO_PARAMETERS = "The parameters can't be null or empty.";
+    private static final String ERROR_NULL_INPUT = "Input can't be null.";
+    private static final String ERROR_NULL_ARRAYS = "Given arrays can't be null.";
     private String apiKey;
     private String secretKey;
+    private boolean isPublic;
+    private static final String ERROR_NULL_METHOD = "The API method can't be null.";
+    private static final String PUBLIC_URL = "https://api.kraken.com/0/public/";
+    private static final String PRIVATE_URL = "https://api.kraken.com/0/private/";
     HMAC256 mac = new HMAC256();
 
-    public KrakenKeys(String apiKey, String secretKey){
+    public KrakenKeys(String apiKey, String secretKey) {
         this.apiKey = apiKey;
         this.secretKey = secretKey;
     }
@@ -35,30 +56,88 @@ public class KrakenKeys extends KrakenClient{
         this.secretKey = secretKey;
     }
 
-    public Long generateNonce(){
-        return System.currentTimeMillis();
+
+
+    public String generateNonce() {
+        return System.currentTimeMillis() + "000";
     }
 
     public String encodeUTF8(String stringToEncode) throws UnsupportedEncodingException {
-        return URLEncoder.encode(stringToEncode,"UTF-8");
+        return URLEncoder.encode(stringToEncode, "UTF-8");
     }
 
-    public String encodeParameters(Map<String,Object> params) throws UnsupportedEncodingException {
+    public String encodeParameters(Map<String, Object> params) throws UnsupportedEncodingException {
         //String postData = params.entrySet().stream().map(e -> urlEncodeUTF8(e.getKey()) + "=" + encode(e.getValue()+"", "UTF-8")).reduce((e1, e2) -> e1 + "&" + e2).orElse("");
         ArrayList<String> encodedParams = new ArrayList<>();
-        for(Map.Entry<String,Object> eachEntry : params.entrySet()){
-            encodedParams.add(encodeUTF8(eachEntry.getKey()) + "=" + encodeUTF8(eachEntry.getValue() + ""));
+        for (Map.Entry<String, Object> eachEntry : params.entrySet()) {
+            encodedParams.add(eachEntry.getKey() + "=" + eachEntry.getValue() + "");
         }
-        return String.join("&",encodedParams);
+        return String.join("&", encodedParams);
     }
 
-    public String generateSignature(String urlPath, Map<String,Object> params) throws UnsupportedEncodingException{
-        String encoded = encodeUTF8(params.get("nonce") + "" + encodeParameters(params));
-        String message = urlPath + mac.hmacDigest(encoded,secretKey,"HmacSHA256");
-        byte[] base64DecodedSecretKey = Base64.getDecoder().decode(secretKey);
-        byte[] result = Base64.getEncoder().encode(mac.hmacDigest(message,new String(base64DecodedSecretKey, StandardCharsets.UTF_8),"HmacSHA512").getBytes());
-        return new String(Base64.getDecoder().decode(result),StandardCharsets.UTF_8);
+    public String generateSignature(Map<String, Object> params) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, MalformedURLException {
+        String nonce = generateNonce();
+        return nonce;
     }
 
+    public static byte[] hmacSha512(byte[] key, byte[] message) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac mac = Mac.getInstance(HMAC_SHA512);
+        mac.init(new SecretKeySpec(key, HMAC_SHA512));
+        return mac.doFinal(message);
+    }
+
+    public static byte[] base64Decode(String input) {
+        return Base64.getDecoder().decode(input);
+    }
+
+    public static String base64Encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    public static byte[] concatArrays(byte[] a, byte[] b) {
+
+        if (a == null || b == null) {
+            throw new IllegalArgumentException(ERROR_NULL_ARRAYS);
+        }
+
+        byte[] concat = new byte[a.length + b.length];
+        for (int i = 0; i < concat.length; i++) {
+            concat[i] = i < a.length ? a[i] : b[i - a.length];
+        }
+
+        return concat;
+    }
+
+    public static byte[] sha256(String message) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance(SHA256);
+        return md.digest(stringToBytes(message));
+    }
+
+    public static byte[] stringToBytes(String input) {
+
+        if (input == null) {
+            throw new IllegalArgumentException(ERROR_NULL_INPUT);
+        }
+
+        return input.getBytes(Charset.forName(UTF8));
+    }
+
+    public static String urlEncode(String input) throws UnsupportedEncodingException {
+        return URLEncoder.encode(input, UTF8);
+    }
+
+    public String setParameters(Map<String, Object> parameters) throws UnsupportedEncodingException {
+
+        if (parameters == null || parameters.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_NO_PARAMETERS);
+        }
+
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String, Object> entry : parameters.entrySet()) {
+            postData.append(entry.getKey()).append(EQUAL_SIGN).append(urlEncode(entry.getValue() + "")).append(AMPERSAND);
+        }
+
+        return postData.toString();
+    }
 
 }
